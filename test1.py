167506f8c5916abb12757d52c6e5da9f1b0c620f
frankiezb <donald.francois001@gmail.com>
2024-02-08 18:31:18 -0600

    update test2

--- test1.py
+++ test1.py
@@ -10,10 +10,13 @@ def say_hi(name):
     print("hi" + name)
         
 def give_me_a_beer(age):
-    if age >=21:
+    if age >=90:
+        print("free drink my boy")
+    elif age >=21:
         print("enjoy")
     else:
-        print("Not getting lit toight buddy")        
+        print("Not getting lit toight buddy")   
+    
         
         
         
